0a9aa449d241c7739c2d5167186a6494fec50f22
Moulik <109475870+moulikmahajan2004@users.noreply.github.com>
2024-04-09 19:51:06 +0530

    Update parallel.cpp

--- M2_T2C/parallel.cpp
+++ M2_T2C/parallel.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <chrono>
-#include <pthread.h>
-
+#include <omp.h>
 using namespace std;
 using namespace std::chrono;
 
@@ -9,14 +8,19 @@ using namespace std::chrono;
 int partition(int A[], int start_index, int end_index)
 {
     int x = A[end_index];
+    
     int i = start_index - 1;
 
+    #pragma omp parallel for
     for (int j = start_index; j <= end_index - 1; j++)
     {
         if (A[j] <= x)
         {
-            i++;
-            swap(A[i], A[j]);
+            #pragma omp critical
+            {
+                i++;
+                swap(A[i], A[j]);
+            }
         }
     }
     swap(A[i + 1], A[end_index]);
@@ -29,34 +33,45 @@ void QUCIKSORT_FUNCTION(int A[], int start_index, int end_index)
     if (start_index < end_index)
     {
         int q = partition(A, start_index, end_index);
+        
+        #pragma omp task
         QUCIKSORT_FUNCTION(A, start_index, q - 1);
+        
+        #pragma omp task
         QUCIKSORT_FUNCTION(A, q + 1, end_index);
     }
 }
 
-const int ARRAYS_SIZE = 10000000;
+const int ARRAYS_SIZE = 1000000;
+
 int main()
 {
     // creating the array and setting the array size according to the need
-    int *array = new int[ARRAYS_SIZE];
+    int* array = new int[ARRAYS_SIZE];
+    //int array[ARRAYS_SIZE];
     srand(time(0));
-    // initializing the random vaklues to the array
+    // initializing the random values to the array
     for (int i = 0; i < ARRAYS_SIZE; ++i)
     {
-        array[i] = rand() % 100;
+        array[i] = rand() % 1000;
     }
     // begin of the chrono
     auto Begin = high_resolution_clock::now();
-    // calulating the time for the array
-    pthread_t thread;
-    int args[] = {array, 0, ARRAYS_SIZE - 1};
-    pthread_create(&thread, NULL, QUCIKSORT_FUNCTION, (void *)args);
-    pthread_join(thread, NULL);
+    
+    // parallel region for sorting
+    #pragma omp parallel
+    {
+        #pragma omp single
+        QUCIKSORT_FUNCTION(array, 0, ARRAYS_SIZE - 1);
+    }
+
     // track of the time at which end
     auto end = high_resolution_clock::now();
-    // duartion is the end - begin and it will tell weathe we  want it in milliseconds or mircoseconds
+    // duration is the end - begin and it will tell whether we want it in milliseconds or microseconds
     auto durations = duration_cast<milliseconds>(end - Begin);
-    // printingthe time
-    cout << "Quick sort sequential time taken -->" << durations.count() << " MilliSec" << endl;
+    // printing the time
+    cout << "Quick sort parallel time taken -->" << durations.count() << " MilliSec" << endl;
+
+    delete[] array; // Don't forget to free the allocated memory
     return 0;
 }
